338b406202b0db5e55033c562d4b0322e58212c6
Stanton <86133203+aerotron1@users.noreply.github.com>
2022-02-25 16:33:29 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<!-- the skeloton and repo -->
---
+++ app.py
@@ -0,0 +1,27 @@
+# imports
+import dash
+from dash import html
+
+
+# setup
+app = dash.Dash(__name__, title='My First Dash Deployment')
+
+
+# Layout
+app.layout = html.Div([
+    html.H3('Consumer Behaviour Dashboard', style={
+        'color': 'green',
+        'padding-top': '60px',
+        'text-align': 'center'
+    }),
+    html.P('Dashboard to follow soon...', style={
+        'color': 'blue',
+        'text-align': 'center',
+        'padding-top': '60px'
+    })
+])
+
+
+# run
+if __name__ == '__main__':
+    app.run_server(debug=True)
